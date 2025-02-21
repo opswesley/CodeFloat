a49e95400ee1e4161a8383910d418d8cd0561745
Wesley <114640242+opswesley@users.noreply.github.com>
2025-02-21 01:20:31 -0300

    server.js

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-// Endpoint para obter a música atual
 app.get('/current-track', (req, res) => {
   const scriptPath = path.join(__dirname, 'get-spotify-info.ps1');
 
@@ -17,14 +16,14 @@ app.get('/current-track', (req, res) => {
     }
 
     try {
-      // Converte a saída do PowerShell em JSON
+      
       const output = stdout.trim();
       const parsedOutput = JSON.parse(output);
 
       if (parsedOutput.message) {
         res.json({ message: parsedOutput.message });
       } else {
-        // Retorna os dados da música
+     
         res.json({
           name: parsedOutput.name || 'Desconhecido',
           artist: parsedOutput.artist || 'Desconhecido',
@@ -37,15 +36,13 @@ app.get('/current-track', (req, res) => {
   });
 });
 
-// Servir arquivos estáticos da raiz do projeto
+
 app.use(express.static(__dirname));
 
-// Redirecionar para index.html se nenhum arquivo for encontrado
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Iniciar o servidor
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
